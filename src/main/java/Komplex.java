7b638f0404b2b50c64f4f77480b716133ba2e002
zahidari <mzhdar1@gmail.com>
2025-03-09 11:16:54 +0100

    first commit

---
+++ src/main/java/Komplex.java
@@ -0,0 +1,26 @@
+public class Komplex {
+    public int x;   // reelteil
+    public int y; // immaginer teil
+
+    public Komplex(){
+
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public void addiere(Komplex z){
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    void testAddition(){
+        int sum = 2+3;
+        System.out.println(sum);
+
+    }
+}
